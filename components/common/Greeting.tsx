e243f7a512970503e506e8b9a6d517fe16e69e9b
abi-hong <flffjakfwm20@gmail.com>
2023-10-05 17:07:07 +0900

    feat: 환영 페이지 UI - #2

---
+++ components/common/Greeting.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import styled from 'styled-components'
+import { GiPeach, GiHamburgerMenu } from 'react-icons/gi'
+
+interface GreetingProps {
+  children: React.ReactNode
+  buttons?: string[]
+  link?: string
+}
+
+export default function Greeting(props: GreetingProps) {
+  const { children, buttons, link } = props
+
+  return (
+    <Styled.Root>
+      <Styled.Header>
+        <Styled.Button>
+          <GiPeach size={40} />
+        </Styled.Button>
+        <Styled.Button>
+          <GiHamburgerMenu size={30} />
+        </Styled.Button>
+      </Styled.Header>
+
+      <Styled.Main>{children}</Styled.Main>
+
+      <Styled.Footer>
+        {buttons && <Styled.StrButton>{buttons[0]}</Styled.StrButton>}
+        {link && <Styled.Link>{link}</Styled.Link>}
+      </Styled.Footer>
+    </Styled.Root>
+  )
+}
+
+const Styled = {
+  Root: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    width: 100%;
+    height: 100vh;
+
+    overflow-y: scroll;
+    overflow-x: hidden;
+  `,
+  Header: styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    margin-top: 1.4rem;
+  `,
+  Button: styled.button`
+    background: inherit;
+    border: none;
+    box-shadow: none;
+    border-radius: 0;
+    padding: 0;
+    overflow: visible;
+    cursor: pointer;
+  `,
+  Main: styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+  `,
+  Footer: styled.footer`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    min-width: 32rem;
+    margin-bottom: 1.2rem;
+  `,
+  StrButton: styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    padding: 1rem;
+    border-radius: 0.6rem;
+    background: black;
+
+    text-align: center;
+    color: white;
+    font-size: 1.3rem;
+    font-weight: 700;
+    letter-spacing: 0.052rem;
+    cursor: pointer;
+  `,
+  Link: styled.a``,
+}
