c3134ef35e0151f8e2cd3e860c72045358e2a82a
abi-hong <flffjakfwm20@gmail.com>
2023-10-05 17:06:50 +0900

    feat: 가능시간 페이지 UI - #2

---
+++ pages/available/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../components/common/Layout'
+
+export default function Available() {
+  return (
+    <Layout buttons={['다음']} header='가능 시간 입력'>
+      
+    </Layout>
+  )
+}
