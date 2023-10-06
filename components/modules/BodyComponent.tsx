696669081e9ca9bd4b7284629f289827e9e7dd59
abi-hong <flffjakfwm20@gmail.com>
2023-10-06 13:11:46 +0900

    feat: Body 컴포넌트 개발 중 - #3

---
+++ components/modules/BodyComponent.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import styled from 'styled-components'
+
+interface BodyProps {}
+
+export default function BodyComponent(props: BodyProps) {
+  return (
+  )
+}
+
+const Styled = {
+
+}
