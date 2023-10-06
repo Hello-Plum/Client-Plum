9a153befd35a798cea099d999b3abe41d4d06489
abi-hong <flffjakfwm20@gmail.com>
2023-10-06 13:11:30 +0900

    chore: add recoil - #3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { RecoilRoot } from 'recoil'
 import MobileWrapper from '../styles/MobileWrapper'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MobileWrapper>
-      <Component {...pageProps} />
-    </MobileWrapper>
+    <RecoilRoot>
+      <MobileWrapper>
+        <Component {...pageProps} />
+      </MobileWrapper>
+    </RecoilRoot>
   )
 }
