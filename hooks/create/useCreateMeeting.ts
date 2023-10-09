6593a1914c07375b29d1761b3a89016e968fc86d
abi-hong <flffjakfwm20@gmail.com>
2023-10-09 14:49:48 +0900

    feat: 회의 생성하기 custom hook 개발 - #3

--- hooks/create/useCreateMeeting.ts
+++ hooks/create/useCreateMeeting.ts
@@ -1,5 +1,40 @@
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { createMeetingStepState, meetingInfoState } from '../../states/atom'
+import { useRouter } from 'next/router'
+import { MeetingInfo } from '../../types/create/createMeetingInterface'
+
 export const useCreateMeeting = () => {
-  const setMeetingButton = () => {
-    
+  // Recoil 상태값 반환 (읽기 전용)
+  const [step, setStep] = useRecoilState(createMeetingStepState)
+  const [meetingInfo, setMeetingInfo] = useRecoilState(meetingInfoState)
+  const router = useRouter()
+
+  const setMeetingInfoForm = (input: Partial<MeetingInfo>) => {
+    setMeetingInfo({ ...meetingInfo, ...input })
+    console.log('setMeetingInfoForm', input)
   }
+
+  const handleBackBtnClick = () => {
+    // 전 step으로 가도록
+    if (step === 0) {
+      router.push('/')
+    } else {
+      setStep(step - 1)
+      console.log('handleBackBtnClick step', step)
+    }
+  }
+  const handleBtnClick = () => {
+    // step + 1 되도록
+    if (step >= 6) {
+      // 마지막 step에서 누를 경우
+      // 1. 서버로 데이터 전송
+      // 2. '/'로 이동
+      router.push('/')
+    } else {
+      setStep(step + 1)
+      console.log('handleBtnClick step', step)
+    }
+  }
+
+  return { step, meetingInfo, setMeetingInfoForm, handleBackBtnClick, handleBtnClick }
 }
--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Layout from '../../components/common/Layout'
-import { useRouter } from 'next/router'
 import CreateMeetingTitleComponent from './CreateMeetingTitleComponenet'
 import { createMeetingSteps } from '../../data/create/createMeetingData'
 import { useCreateMeeting } from '../../hooks/create/useCreateMeeting'
 import CreateMeetingBodyComponent from './CreateMeetingBodyComponent'
 
 export default function Create() {
-  const router = useRouter()
-  const [step, setStep] = useState(0)
+  const { step, meetingInfo, setMeetingInfoForm, handleBackBtnClick, handleBtnClick } = useCreateMeeting()
   const currentStep = createMeetingSteps[step]
-  //const { setMeetingButton } = useCreateMeeting
+  
 
   return (
     <Layout 
       buttons={['다음']} 
       header='회의 정보 입력'
-      onClickBackButton={() => router.push('/')}
-      onClickButton={() => {
-        setStep(step+1)
-      }}
+      onClickBackButton={handleBackBtnClick}
+      onClickButton={handleBtnClick}
     >
       <CreateMeetingTitleComponent step={currentStep}/>
-      <CreateMeetingBodyComponent />
+      <CreateMeetingBodyComponent 
+        currentStep={currentStep}
+        meetingInfo={meetingInfo}
+        setMeetingInfo={setMeetingInfoForm}
+      />
     </Layout>
   )
 }
