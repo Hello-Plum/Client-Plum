97d69c2015749e1a234d703af9c8346f7f503000
abi-hong <flffjakfwm20@gmail.com>
2023-10-17 20:55:21 +0900

    refactor: 주, 시간 리스트 데이터 분리 - #4

---
+++ data/available/checkAvailableTimeTableData.ts
@@ -0,0 +1,8 @@
+// Array.prototype.at() -> 음수인덱스 사용가능
+export const weekList: Array<string> = [
+  '월', '화', '수', '목', '금', '토', '일'
+]
+
+export const timeList: Array<string> = [
+  '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM', '12AM'
+]
