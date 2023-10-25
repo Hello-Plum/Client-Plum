d6f07430111bb7c0ccc76b294a13056ccae43073
abi-hong <flffjakfwm20@gmail.com>
2023-10-25 19:12:22 +0900

    feat: react-query 적용 - #6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,19 @@ import './create/components/SetAvailableDates.css'
 import './enter/available/components/SetAvailableTimeTable.css'
 import { RecoilRoot } from 'recoil'
 import MobileWrapper from '../styles/MobileWrapper'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
     <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
       <MobileWrapper>
         <Component {...pageProps} />
       </MobileWrapper>
+      </QueryClientProvider>
     </RecoilRoot>
   )
 }
