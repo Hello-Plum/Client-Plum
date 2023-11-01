4b51850c14266c46ce7fd4461fdc39d68950262c
abi-hong <flffjakfwm20@gmail.com>
2023-11-01 13:43:06 +0900

    feat: 카드 공통 컴포넌트 개발 - #8

---
+++ components/atom/CardComponent.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ShowMeetingInfo } from '../../types/cardInfo/showInfoInterface'
+
+interface CardProps {
+  cardInfo: Partial<ShowMeetingInfo>
+}
+
+export default function CardComponent(props: CardProps) {
+  const { cardInfo } = props
+  return (
+    <Styled.CardWrapper>
+      <Styled.Title>{cardInfo.name}</Styled.Title>
+      <Styled.ListWrapper>
+        <Styled.List>{`${cardInfo.startDate} ~ ${cardInfo.endDate}`}</Styled.List>
+        <Styled.List>{cardInfo.place}</Styled.List>
+      </Styled.ListWrapper>
+    </Styled.CardWrapper>
+  )
+}
+
+const Styled = {
+  CardWrapper : styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 3rem;
+
+    width: 100%;
+    height: 100%;
+    background-color: rgb(244, 248, 255);
+  `,
+  Title : styled.h1`
+    font-size: 1.9rem;
+    font-weight: 550;
+    letter-spacing: -0.03rem;
+  `,
+  ListWrapper : styled.ul`
+  `,
+  List : styled.li`
+    color: black;  
+    font-size: 1.5rem;
+    letter-spacing: -0.03rem;
+    padding: 1.5rem 1rem 0px;
+    list-style: inside;
+  `,
+}
---
+++ types/cardInfo/showInfoInterface.ts
@@ -0,0 +1,9 @@
+export interface ShowMeetingInfo {
+  name: string
+  startDate: string
+  endDate: string
+  place: string
+  placeDetail: string
+  host: string
+  info: string
+}
