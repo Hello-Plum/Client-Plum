e708456f9d1bf614ca5d7dee9ea0cfd23c2d4b78
abi-hong <flffjakfwm20@gmail.com>
2023-11-01 13:43:35 +0900

    feat: 생성된 회의 정보 보여주기 뷰 개발 - #8

--- pages/meetingInfo/index.tsx
+++ pages/meetingInfo/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 import Greeting from '../../components/common/Greeting'
+import styled from 'styled-components'
+import CardComponent from '../../components/atom/CardComponent'
 
 export default function MeetingInfo() {
   const router = useRouter()
@@ -8,7 +10,25 @@ export default function MeetingInfo() {
   return (
     // button 누르면 링크 복사
     <Greeting buttons={['링크 공유하기']} onClickButton={() => {}}>
-      생성한 회의 정보 카드 형식으로 보여줌!!
+      <Styled.MeetingInfoWrapper>
+        <Styled.Title>링크를 통해 초대하고 함께 일정을 정하세요!</Styled.Title>
+        <CardComponent cardInfo={{name: '회의명', startDate: '2023-11-1', endDate: '2023-11-5', place: '회의 장소'}}/>
+      </Styled.MeetingInfoWrapper>
     </Greeting>
   )
+}
+
+const Styled = {
+  MeetingInfoWrapper : styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 2rem 1rem; 
+  `,
+  Title : styled.h1`
+    font-size: 1.9rem;
+    font-weight: 550;
+    letter-spacing: -0.03rem;
+    padding: 2rem 0px 4rem;
+  `,
+
 }
