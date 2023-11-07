efd5629564772ab748598fc9227365e4fb7ac495
abi-hong <flffjakfwm20@gmail.com>
2023-11-07 23:46:40 +0900

    refactor: 동적 라우팅 변경

--- pages/enter/[meetingId].tsx
+++ pages/enter/[meetingId].tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Greeting from '../../components/common/Greeting'
-import { useRouter } from 'next/navigation'
-import { useParams } from 'next/navigation'
+import { useRouter } from 'next/router'
 
 export default function Enter() {
   const router = useRouter()
-  const param = useParams()
+  const { meetingId } = router.query
 
   return (
     <Greeting 
       buttons={['팀원 입장하기', '방장 입장하기']}
-      onClickButtonLeft={() => router.push(`/enter/confirm/${param.meetingId}`)} //'방장 입장하기'
-      onClickButton={() => router.push(`/enter/available/${param.meetingId}`)} //'팀원 입장하기'
+      onClickButtonLeft={() => router.push(`/enter/confirm/${meetingId}`)} //'방장 입장하기'
+      onClickButton={() => router.push(`/enter/available/${meetingId}`)} //'팀원 입장하기'
     >
     </Greeting>
   )
