7e2978c34a0a16e96913e89b8be8d7094c07cc04
abi-hong <flffjakfwm20@gmail.com>
2023-11-17 18:56:25 +0900

    refactor: 회의 조회 데이터 인터페이스화 후, 파일 분리 - #4

--- hooks/meetingDetail/useMeetingDetail.ts
+++ hooks/meetingDetail/useMeetingDetail.ts
@@ -1,20 +1,6 @@
 import { useEffect, useState } from "react";
 import { getMeetingDetail } from "../../api/meetingDetailApi";
-
-interface Detail {
-  id: string
-  name: string
-  startDate: string
-  endDate: string
-  place: string
-  placeDetail: string
-  host: string
-  password: string
-  info: string
-  confirmed: boolean
-  period: boolean
-  selectedWeek: Array<number>
-}
+import { Detail } from "../../types/cardInfo/showInfoInterface";
 
 export const useMeetingDetail = (meetingId: string | string[] | undefined) => {
   const [isLoading, setIsLoading] = useState(true)
--- types/cardInfo/showInfoInterface.ts
+++ types/cardInfo/showInfoInterface.ts
@@ -1,9 +1,14 @@
-export interface ShowMeetingInfo {
+export interface Detail {
+  id: string
   name: string
   startDate: string
   endDate: string
   place: string
   placeDetail: string
   host: string
+  password: string
   info: string
+  confirmed: boolean
+  period: boolean
+  selectedWeek: Array<number>
 }
